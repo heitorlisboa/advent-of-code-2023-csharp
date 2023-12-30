88d602382a922c117faa8e46bfcb66d8250fd713
heitorlisboa <heitor01101000@gmail.com>
2023-12-30 01:19:42 -0300

    day 1

---
+++ Day1_a/Program.cs
@@ -0,0 +1,45 @@
+﻿string directory = AppDomain.CurrentDomain.BaseDirectory;
+string filePath = Path.GetFullPath(
+  Path.Combine(directory, "../../../input.txt")
+);
+string[] lines = File.ReadAllLines(filePath);
+
+int sumOfCalibrationValues = 0;
+
+foreach (string line in lines)
+{
+  char? firstNumberAsChar = null;
+  char? lastNumberAsChar = null;
+
+  for (int startIndex = 0; startIndex < line.Length; startIndex++)
+  {
+    char character = line[startIndex];
+    bool isDigit = int.TryParse(character.ToString(), out _);
+
+    if (isDigit)
+    {
+      firstNumberAsChar = character;
+      break;
+    }
+  }
+
+  for (int endIndex = line.Length - 1; endIndex >= 0; endIndex--)
+  {
+    char character = line[endIndex];
+    bool isDigit = int.TryParse(character.ToString(), out _);
+
+    if (isDigit)
+    {
+      lastNumberAsChar = character;
+      break;
+    }
+  }
+
+  if (firstNumberAsChar is null || lastNumberAsChar is null)
+    return;
+
+  int calibrationValue = int.Parse($"{firstNumberAsChar}{lastNumberAsChar}");
+  sumOfCalibrationValues += calibrationValue;
+}
+
+Console.WriteLine(sumOfCalibrationValues.ToString());
---
+++ Day1_b/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+string directory = AppDomain.CurrentDomain.BaseDirectory;
+string filePath = Path.GetFullPath(
+  Path.Combine(directory, "../../../input.txt")
+);
+string[] lines = File.ReadAllLines(filePath);
+
+Regex numberRegex =
+  new(@"(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))");
+
+Dictionary<string, int> valuesOfNumbersSpelledWithLetters =
+  new()
+  {
+    ["one"] = 1,
+    ["two"] = 2,
+    ["three"] = 3,
+    ["four"] = 4,
+    ["five"] = 5,
+    ["six"] = 6,
+    ["seven"] = 7,
+    ["eight"] = 8,
+    ["nine"] = 9,
+  };
+
+int sumOfCalibrationValues = 0;
+
+foreach (string line in lines)
+{
+  MatchCollection matchCollection = numberRegex.Matches(line);
+  string firstDigitAsString = matchCollection.First().Groups[1].Value;
+  int firstDigit = valuesOfNumbersSpelledWithLetters.ContainsKey(
+    firstDigitAsString
+  )
+    ? valuesOfNumbersSpelledWithLetters[firstDigitAsString]
+    : int.Parse(firstDigitAsString);
+
+  string lastDigitAsString = matchCollection.Last().Groups[1].Value;
+  int lastDigit = valuesOfNumbersSpelledWithLetters.ContainsKey(
+    lastDigitAsString
+  )
+    ? valuesOfNumbersSpelledWithLetters[lastDigitAsString]
+    : int.Parse(lastDigitAsString);
+
+  int calibrationValue = int.Parse($"{firstDigit}{lastDigit}");
+  sumOfCalibrationValues += calibrationValue;
+}
+
+Console.WriteLine(sumOfCalibrationValues.ToString());
